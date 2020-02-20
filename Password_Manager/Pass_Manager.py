9b7d87f9cf7a952ee9f550e799c123acc84a0211
Alex Carmichael <alexcarm123@gmail.com>
2020-02-20 11:47:59 -0500

    Finished Commit

--- Password_Manager/Pass_Manager.py
+++ Password_Manager/Pass_Manager.py
@@ -78,4 +78,144 @@ class entity_add:
 
         f.write(encryptedN + ',' + encryptedE + ',' + encryptedP + ',\n')
         f.close()
-    
+
+#Class that displays stored passwords after login
+class entity_display:
+
+    def __init__(self, master, n, e, p, i):
+        self.password = p
+        self.name = n
+        self.username = e
+        self.window = master
+        self.i = i
+
+        dencryptedN = ""
+        dencryptedE = ""
+        dencryptedP = ""
+        for letter in self.name:
+            if letter == ' ':
+                dencryptedN += ' '
+            else:
+                dencryptedN += chr(ord(letter) - 5)
+
+        for letter in self.username:
+            if letter == ' ':
+                dencryptedE += ' '
+            else:
+                dencryptedE += chr(ord(letter) - 5)
+
+        for letter in self.password:
+            if letter == ' ':
+                dencryptedP += ' '
+            else:
+                dencryptedP += chr(ord(letter) - 5)
+
+        self.label_name = Label(self.window, text=dencryptedN, font=('Courier', 14))
+        self.label_username = Label(self.window, text=dencryptedE, font=('Courier', 14))
+        self.label_pass = Label(self.window, text=dencryptedP, font=('Courier', 14))
+        self.deleteButton = Button(self.window, text='X', fg='red', command=self.delete)
+
+    def display(self):
+        self.label_name.grid(row=6 + self.i, sticky=W)
+        self.label_username.grid(row=6 + self.i, column=1)
+        self.label_pass.grid(row=6 + self.i, column=2, sticky=E)
+        self.deleteButton.grid(row=6 +self.i, column=3, sticky=E)
+
+    def delete(self):
+        answer = tkinter.messagebox.askquestion('Delete', 'Are you sure you want to delete this entry?')
+
+        if answer == 'yes':
+            for i in objects:
+                i.destroy()
+
+            f = open('users.txt', 'r')
+            lines = f.readlines()
+            f.close()
+
+            f = open('users.txt', "w")
+            count = 0
+
+            for line in lines:
+                if count != self.i:
+                    f.write(line)
+                    count += 1
+
+            f.close()
+            readfile()
+
+    def destroy(self):
+        self.label_name.destroy()
+        self.label_username.destroy()
+        self.label_pass.destroy()
+        self.deleteButton.destroy()
+
+
+# ******* FUNCTIONS *********
+
+
+def onsubmit():
+    m = username.get()
+    p = password.get()
+    n = name.get()
+    e = entity_add(window, n, p, m)
+    e.write()
+    name.delete(0, 'end')
+    username.delete(0, 'end')
+    password.delete(0, 'end')
+    messagebox.showinfo('Added Entity', 'Successfully Added, \n' + 'Name: ' + n + '\nUsername: ' + m + '\nPassword: ' + p)
+    readfile()
+
+
+def clearfile():
+    f = open('users.txt', "w")
+    f.close()
+
+
+def readfile():
+    f = open('users.txt', 'r')
+    count = 0
+
+    for line in f:
+        entityList = line.split(',')
+        e = entity_display(window, entityList[0], entityList[1], entityList[2], count)
+        objects.append(e)
+        e.display()
+        count += 1
+    f.close()
+
+
+# ******* GRAPHICS *********
+
+m = popupWindow(window)
+
+entity_label = Label(window, text='Add Entity', font=('Courier', 18))
+name_label = Label(window, text='Name: ', font=('Courier', 14))
+username_label = Label(window, text='Username: ', font=('Courier', 14))
+pass_label = Label(window, text='Password: ', font=('Courier', 14))
+name = Entry(window, font=('Courier', 14))
+username = Entry(window, font=('Courier', 14))
+password = Entry(window, show='*', font=('Courier', 14))
+submit = Button(window, text='Add User', command=onsubmit, font=('Courier', 14))
+
+entity_label.grid(columnspan=3, row=0)
+name_label.grid(row=1, sticky=E, padx=3)
+username_label.grid(row=2, sticky=E, padx=3)
+pass_label.grid(row=3, sticky=E, padx=3)
+
+name.grid(columnspan=3, row=1, column=1, padx=2, pady=2, sticky=W)
+username.grid(columnspan=3, row=2, column=1, padx=2, pady=2, sticky=W)
+password.grid(columnspan=3, row=3, column=1, padx=2, pady=2, sticky=W)
+
+submit.grid(columnspan=3, pady=4)
+
+name_label2 = Label(window, text='Name: ', font=('Courier', 14))
+username_label2 = Label(window, text='Username: ', font=('Courier', 14))
+pass_label2 = Label(window, text='Password: ', font=('Courier', 14))
+
+name_label2.grid(row=5)
+username_label2.grid(row=5, column=1)
+pass_label2.grid(row=5, column=2)
+
+readfile()
+
+window.mainloop()
